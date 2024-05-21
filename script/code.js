1adca506fa0f52da4bec6b43a755a66984243347
ImaraanSheldon <imaraansheldonis2302003@gmail.com>
2024-05-21 11:26:32 +0200

    js CSS code switch

---
+++ script/code.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let checkbox = document.getElementById('chek');
+    checkbox.addEventListener('change', function () {
+        if (this.checked) {
+            swapCSS('css/styleDark.css');
+        } else {
+            swapCSS('css/style.css');
+        }
+    });
+});
+
+function swapCSS(newCSS) {
+    let stylesheet = document.getElementById('stylesheet');
+    stylesheet.href = newCSS;
+}
