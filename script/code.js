04176296b4dac44837eee963a1058bcf9bf2da25
ImaraanSheldon <imaraansheldonis2302003@gmail.com>
2024-06-14 01:15:33 -0700

    Allahu-akbra

--- script/code.js
+++ script/code.js
@@ -33,7 +33,312 @@ button_View
 id
 : 
 1
-  }
+  },
+  {artwork_Artist
+    : 
+    "Yuri Kortov",
+    artwork_Description
+    : 
+    "French Siesta by Yuri Kortov is a tranquil and charming portrayal of a quintessential French scene, encapsulating the essence of leisure and relaxation. With vibrant colors and playful brushstrokes, Kortov captures the idyllic atmosphere of a sun-drenched café, where patrons enjoy a moment of respite from the bustle of everyday life. The warm sunlight filters through the foliage, casting dappled shadows on the cobblestone streets and adding to the scene`s inviting allure. Through his skillful composition and attention to detail, Kortov invites viewers to immerse themselves in the scene.",
+    artwork_Img
+    : 
+    "https://imaraansheldon.github.io/allImages/ColorMyWorld/Impressionism/frenchSiesta_YuriKortov.jpg",
+    artwork_Name
+    : 
+    "French Siesta",
+    artwork_Price
+    : 
+    3200,
+    artwork_Target
+    : 
+    "Highlights",
+    artwork_Theme
+    : 
+    "Impressionism",
+    button_Add
+    : 
+    "Add to Cart",
+    button_View
+    : 
+    "View Artwork",
+    id
+    : 
+    2},
+    {artwork_Artist
+      : 
+      "Yuri Kortov",
+      artwork_Description
+      : 
+      "Catherine La Rose by Yuri Kortov is a captivating art piece that transcends traditional boundaries with its unique blend of surrealism and romanticism. In this mesmerizing composition, Kortov skillfully intertwines elements of dreamlike landscapes with intricate floral motifs, creating a sense of enchantment and mystery. The central figure, Catherine, exudes an aura of elegance and grace, her presence accentuated by the delicate roses that envelop her form. Through masterful use of color and texture, Kortov imbues the piece with a sense of ethereal beauty, inviting viewers into a world where reality and fantasy converge. Catherine La Rose stands as a testament to Kortov`s artistic vision and his ability to evoke emotions and provoke contemplation through his evocative imagery.",
+      artwork_Img
+      : 
+      "https://imaraansheldon.github.io/allImages/ColorMyWorld/Impressionism/CatherineLaRose_YuriKortov.jpg",
+      artwork_Name
+      : 
+      "Catherine La Rose",
+      artwork_Price
+      : 
+      3000,
+      artwork_Target
+      : 
+      "Highlights",
+      artwork_Theme
+      : 
+      "Impressionism",
+      button_Add
+      : 
+      "Add to Cart",
+      button_View
+      : 
+      "View Artwork",
+      id
+      : 
+      3},
+      {artwork_Artist
+        : 
+        "Illyna Samaai",
+        artwork_Description
+        : 
+        "In Illyna Samaai`s expressive portrayal, a woman enveloped in a vibrant hijab emerges as a symbol of concealed resilience and profound emotion. With bold strokes and vivid hues characteristic of expressivism, Samaai captures the enigmatic allure of the veiled figure, whose obscured face hints at inner depths yet to be explored. The hijab, adorned with a riot of colors, becomes a dynamic expression of identity and defiance, shrouding the woman`s features while imbuing her presence with an undeniable sense of strength and purpose. Through this striking composition, Samaai invites viewers to contemplate the complexities of cultural identity and the silent struggles faced by individuals navigating the intersection of tradition and personal agency.",
+        artwork_Img
+        : 
+        "https://imaraansheldon.github.io/allImages/ColorMyWorld/Expressivism/nurAin.jpg",
+        artwork_Name
+        : 
+        "Nur Ain",
+        artwork_Price
+        : 
+        2900,
+        artwork_Target
+        : 
+        "Highlights",
+        artwork_Theme
+        : 
+        "Expressivism",
+        button_Add
+        : 
+        "Add to Cart",
+        button_View
+        : 
+        "View Artwork",
+        id
+        : 
+        4
+        },
+        {artwork_Artist
+          : 
+          "Mihle Lingani",
+          artwork_Description
+          : 
+          "The artwork titled Miho by Mihle Lingani is a vibrant, colorful depiction of a person wearing sunglasses and a suit jacket with a prominent lapel. The piece features bold, contrasting colors such as bright shades of blue, green, red, yellow, and pink, set against a solid peach background. The use of color is striking, with the different hues creating a dynamic and energetic feel. The style is reminiscent of pop art, characterized by its use of flat, vivid colors and a focus on modern, fashionable subjects. The person depicted has white hair and is shown in a confident, stylish pose, giving the piece a contemporary and trendy vibe..",
+          artwork_Img
+          : 
+          "https://imaraansheldon.github.io/allImages/ColorMyWorld/Graphic_Design/MihleLingani_2.jpg",
+          artwork_Name
+          : 
+          "Miho",
+          artwork_Price
+          : 
+          1500,
+          artwork_Target
+          : 
+          "showcase",
+          artwork_Theme
+          : 
+          "Pop Art",
+          button_Add
+          : 
+          "Add to Cart",
+          button_View
+          : 
+          "View Artwork",
+          id
+          : 
+          5},
+          {
+            artwork_Artist
+            : 
+            "Mihle Lingani",
+            artwork_Description
+            : 
+            "Dante by Mihle Lingani is a bold and captivating abstract artwork featuring a close-up of a face with a focus on the eye, nose, and mouth. The piece uses a vivid palette of pink, orange, green, yellow, and dark blue against a bright pink background, creating a dynamic and intense visual effect. The swirling lines and distinct patches of color convey depth and emotion, resulting in a dramatic and contemporary aesthetic that draws the viewer in.",
+            artwork_Img
+            : 
+            "https://imaraansheldon.github.io/allImages/ColorMyWorld/Graphic_Design/MihleLingani_3.jpg",
+            artwork_Name
+            : 
+            "Dante",
+            artwork_Price
+            : 
+            1500,
+            artwork_Target
+            : 
+            "showcase",
+            artwork_Theme
+            : 
+            "Contemporary Art",
+            button_Add
+            : 
+            "Add to Cart",
+            button_View
+            : 
+            "View Artwork",
+            id
+            : 
+            6},
+            {artwork_Artist
+              : 
+              "Mihle Lingani",
+              artwork_Description
+              : 
+              "The Vega Vine Front Cover by Mihle Lingani is a vibrant, modern magazine cover featuring a bold graphic illustration of a person with long hair and large pink sunglasses. Set against a deep purple background with geometric shapes, the cover includes striking text elements like DON'T SETTLE and VEGA VINE, emphasizing themes of collaboration and creative challenges. The design, marked by vivid colors and contemporary typography, creates an engaging and dynamic visual appeal, making it an eye-catching introduction to the magazine`s innovative content.",
+              artwork_Img
+              : 
+              "https://imaraansheldon.github.io/allImages/ColorMyWorld/Graphic_Design/MihleLingani_4.jpg",
+              artwork_Name
+              : 
+              "Miho",
+              artwork_Price
+              : 
+              1500,
+              artwork_Target
+              : 
+              "showcase",
+              artwork_Theme
+              : 
+              "Pop Art",
+              button_Add
+              : 
+              "Add to Cart",
+              button_View
+              : 
+              "View Artwork",
+              id
+              : 
+              7},
+              {artwork_Artist
+                : 
+                "Mihle Lingani",
+                artwork_Description
+                : 
+                "Heat by Mihle Lingani depicts a man breathing cold air upwards, set against a dark grey background. His white hair contrasts sharply with the fiery oranges, reds, and greens that dominate the artwork, evoking a sense of intense warmth and passion. The cold breath forms a striking focal point, symbolizing relief or contemplation amidst the surrounding heat. This vivid interplay of colors and elements captures the dualities of life, inviting viewers to explore the balance between chaos and calm, and passion and reflection.",
+                artwork_Img
+                : 
+                "https://imaraansheldon.github.io/allImages/ColorMyWorld/Graphic_Design/MihleLingani_1.jpg",
+                artwork_Name
+                : 
+                "Heat",
+                artwork_Price
+                : 
+                1500,
+                artwork_Target
+                : 
+                "showcase",
+                artwork_Theme
+                : 
+                "Contemporary Art",
+                button_Add
+                : 
+                "Add to Cart",
+                button_View
+                : 
+                "View Artwork",
+                id
+                : 
+                8},
+                {artwork_Artist
+                  : 
+                  "Mihle Lingani",
+                  artwork_Description
+                  : 
+                  "In this captivating artwork, a woman with a voluminous afro stares directly at the viewer, her gaze unwavering and powerful. The portrait captures her from the chest up, emphasizing her strong presence and commanding expression. Her eyes, deep and expressive, seem to tell a story of resilience and strength. The texture of her hair is meticulously detailed, each curl adding to the sense of depth and realism. The background is kept minimal, ensuring that all attention is drawn to her face and hair. The lighting subtly highlights her features, creating a striking contrast that enhances the intensity of her gaze. This portrait not only celebrates natural beauty but also conveys a profound sense of identity and empowerment.",
+                  artwork_Img
+                  : 
+                  "https://imaraansheldon.github.io/allImages/ColorMyWorld/Graphic_Design/MihleLingani_5.jpg",
+                  artwork_Name
+                  : 
+                  "Air",
+                  artwork_Price
+                  : 
+                  1500,
+                  artwork_Target
+                  : 
+                  "All Products",
+                  artwork_Theme
+                  : 
+                  "Contemporary Art",
+                  button_Add
+                  : 
+                  "Add to Cart",
+                  button_View
+                  : 
+                  "View Artwork",
+                  id
+                  : 
+                  9},
+                  {
+                    
+artwork_Artist
+: 
+"Ismaa`eel Ahmed",
+artwork_Description
+: 
+"In this captivating artwork, a woman with a voluminous afro stares directly at the viewer, her gaze unwavering and powerful. The portrait captures her from the chest up, emphasizing her strong presence and commanding expression. Her eyes, deep and expressive, seem to tell a story of resilience and strength. The texture of her hair is meticulously detailed, each curl adding to the sense of depth and realism. The background is kept minimal, ensuring that all attention is drawn to her face and hair. The lighting subtly highlights her features, creating a striking contrast that enhances the intensity of her gaze. This portrait not only celebrates natural beauty but also conveys a profound sense of identity and empowerment.",
+artwork_Img
+: 
+"https://imaraansheldon.github.io/allImages/ColorMyWorld/Expressivism/herInAfrica.jpg",
+artwork_Name
+: 
+"Her in Africa",
+artwork_Price
+: 
+7000,
+artwork_Target
+: 
+"All Products",
+artwork_Theme
+: 
+"Contemporary Art",
+button_Add
+: 
+"Add to Cart",
+button_View
+: 
+"View Artwork",
+id
+: 
+10
+  },
+  {artwork_Artist
+    : 
+    "Rene Margitte",
+    artwork_Description
+    : 
+    "\"Académie Morbide\" by René Magritte is a surrealistic painting that juxtaposes the familiar and the bizarre to provoke thought and challenge perceptions. The artwork features a human torso, distinctly anatomical and somewhat unsettling, adorned with various surreal elements that are characteristic of Magritte's style. These elements often include disjointed body parts, incongruous objects, or mysterious landscapes that evoke a sense of unease and curiosity. Magritte's use of such imagery encourages viewers to question the nature of reality and the boundaries between the ordinary and the extraordinary, encapsulating his unique ability to blend the mundane with the morbidly fascinating.",
+    artwork_Img
+    : 
+    "https://imaraansheldon.github.io/allImages/ColorMyWorld/Art/AcademiqueMorbide_ReneMargitte.jpg",
+    artwork_Name
+    : 
+    "Academique Morbide",
+    artwork_Price
+    : 
+    9000,
+    artwork_Target
+    : 
+    "All Products",
+    artwork_Theme
+    : 
+    "Surrealism",
+    button_Add
+    : 
+    "Add to Cart",
+    button_View
+    : 
+    "View Artwork",
+    id
+    : 
+    11}
 ];
 
 // Save products to localStorage
